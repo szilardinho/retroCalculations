61c1945c12d70c1fed2112317bcd4ae266b229b8
szilardinho <szilardinhog@gmail.com>
2017-03-30 19:25:48 -0400

    Outlets..need to update to collections

--- retroCalculations/ViewController.swift
+++ retroCalculations/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var buttonSound: AVAudioPlayer!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            try buttonSound = AVAudioPlayer(contentsOf: soundURL)
+        } catch let error as NSError {
+            print( (error.debugDescription))
+        }
+    }
+    
+    @IBAction func numberPressed(sender: UIButton){
+        
+        playSound()
+        
+    }
+    
+    func playSound()
+    {
+        if buttonSound.isPlaying {
+            buttonSound.stop()
+        }
+        
+        buttonSound.play()
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
